6178f934691a926357408f3ed838b5bc56cbc39f
Nethra <112483364+Dneth04@users.noreply.github.com>
2024-09-14 21:58:30 +0400

    donedone

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Solutions from "./Solutions";
 import CTA from "./CTA";
 import Stats from "./Stats";
-import Footer from "./Footer";
+import Footer from "./footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 import Tools from "./Tools"
